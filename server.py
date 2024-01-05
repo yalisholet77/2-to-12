0b9c86073a2911e86c7ac67b31f99900297f188b
yali teater‬‏ <134304770+yalisholet77@users.noreply.github.com>
2024-01-05 13:00:40 +0200

    Update server.py

--- server.py
+++ server.py
@@ -7,16 +7,25 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         if self.path == '/':
             self.path = '/index.html'
 
+        # Setting the content type
+        if self.path.endswith(".html"):
+            content_type = 'text/html'
+        elif self.path.endswith(".css"):
+            content_type = 'text/css'
+        elif self.path.endswith(".js"):
+            content_type = 'application/javascript'
+        else:
+            content_type = 'text/plain'
+
         try:
             # Reading the file content
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
-
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+            with open(self.path[1:], 'rb') as file:  # Open in binary mode for universal compatibility
+                self.send_response(200)
+                self.send_header('Content-type', content_type)
+                self.end_headers()
+                self.wfile.write(file.read())
+        except FileNotFoundError:
+            self.send_error(404, "File not found")
 
 # Specify the port
 port = 8000
